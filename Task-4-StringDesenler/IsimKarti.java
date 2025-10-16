f13fb29ee9afcd73f0a0dd3c70a73c3f2fcfcdaf
Akif099 <250542011@firat.edu.tr>
2025-10-16 22:14:27 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,17 @@
+public class IsimKarti {
+2 public static void main ( String [] args ) {
+3 String ad = " Ahmet ";
+4 String soyad = " Yilmaz ";
+5 String ogrNo = " 220301010 ";
+6 String bolum = " Yazilim Muh .";
+7
+8 System . out . println (" =================== ") ;
+9 System . out . println (" OGRENCI BILGILERI ") ;
+0 System . out . println (" =================== ") ;
+ System . out . println ("Ad : " + ad ) ;
+2 System . out . println (" Soyad : " + soyad ) ;
+3 System . out . println ("No : " + ogrNo ) ;
+4 System . out . println (" Bolum : " + bolum ) ;
+5 System . out . println (" =================== ") ;
+6 }
+7 }
