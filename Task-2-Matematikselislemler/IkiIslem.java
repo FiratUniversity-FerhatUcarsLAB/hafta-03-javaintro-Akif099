2325756a9ec81736da603592ea5ecfcf889b3759
Akif099 <250542011@firat.edu.tr>
2025-10-16 22:09:55 +0300

    Update IkiIslem.java

---
+++ Task-2-Matematikselislemler/IkiIslem.java
@@ -0,0 +1,16 @@
+ public class IkiIslem {
+2 public static void main ( String [] args ) {
+3 // Pay hesapla
+4 double pay = (9.5*4.5) - (2.5*3) ;
+5
+6 // Payda hesapla
+7 double payda = 45.5 - 3.5;
+8
+9 // Bolme yap
+0 double sonuc = pay / payda ;
+
+2 // Formatla ve yazdir
+3 System . out . printf ("%.4 f\n", sonuc ) ;
+4 }
+5 }
+Onemli: ¨
